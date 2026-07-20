4b9d670c8ab275722bbdf2e0ed7cc3fa7de093a8
Aurélie Beaufils <lapaille17@hotmail.fr>
2026-07-20 11:08:56 +0200

    amelioration performance

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,44 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import App from "./App.jsx";
+// import Home from "./pages/Home.jsx";
+// import ProjectDetail from "./pages/ProjectDetail.jsx";
+
+// export default function Router() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<App />}>
+//           <Route index element={<Home />} />
+//           <Route path="projects/:id" element={<ProjectDetail />} />
+//           <Route path="*" element={<Home />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import Home from "./pages/Home.jsx";
-import ProjectDetail from "./pages/ProjectDetail.jsx";
+import { lazy, Suspense } from "react";
+
+// Lazy loading du layout
+const App = lazy(() => import("./App.jsx"));
+
+// Lazy loading des pages
+const Home = lazy(() => import("./pages/Home.jsx"));
+const ProjectDetail = lazy(() => import("./pages/ProjectDetail.jsx"));
 
 export default function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="projects/:id" element={<ProjectDetail />} />
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div className="loader">Chargement...</div>}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="projects/:id" element={<ProjectDetail />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import Router from "./Router.jsx";
+
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//      <Router />
+//   </StrictMode>,
+// )
+
+
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import Router from "./Router.jsx";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <Router />
-  </StrictMode>,
-)
-
-
+    <Suspense fallback={<div className="loader">Chargement...</div>}>
+      <Router />
+    </Suspense>
+  </StrictMode>
+);
